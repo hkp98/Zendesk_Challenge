13b4529657aeab1f02423fd0f565837ee48a65df
hkp98 <hkp49@scarletmail.rutgers.edu>
2021-07-31 17:35:43 -0400

    Creating Application View

---
+++ view.py
@@ -0,0 +1,49 @@
+import math 
+
+class ApplicationView():
+    def __init__(self):
+        self.page_limit = 25
+        self.errorCode = None
+    
+    def mainMessage(self): # display the intial view of Application
+        print("********** Welcome to Zendesk Ticket Viewer Interface **********")
+        print(" The Interface lets the User to view tickets on a particular Zendesk Account ")
+        print(" Type menu to view the options or Type q to exit the Application: ")
+        return 0
+    
+    def displayErrors(self,error_message: str):
+        if self.errorCode is not None:
+            print("\nError fetching data form the API: ",self.errorCode)
+        print(error_message)
+        return 1
+    
+    def displayInputMessage(self,message: str,code: int):
+        print(message)
+        return code
+    
+    def printMenu(self):
+        print("\nSelect View Options")
+        print("Press 1 to print all tickets")
+        print("Press 2 to print a single ticket information")
+        print("Press q to quit the application")
+        print("Type 'menu' to display view options")
+        print("Enter your choice: ")
+        return 0
+    
+    def quit(self):
+        print("\n..Thank you for using the Zendesk Ticket Viewer...")
+        return 0
+    
+    def fetchTickets(self,ticketID):
+        if ticketID == "all":
+            print("\n Fetching all the tickets, please wait ...")
+        else: 
+            print("\nFething the ticket", ticketID + ",","please wait ...")
+        return 0
+    
+    def printTickets(self,tickets_data,pageNO):
+        tickets = tickets_data["tickets"]
+         
+
+
+

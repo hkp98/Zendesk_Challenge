8ef0d153a6c3693f02064b855a13ccd5b76c4d43
hkp98 <hkp49@scarletmail.rutgers.edu>
2021-08-01 16:03:31 -0400

    Application Updates

--- presenter/presenter.py
+++ presenter/presenter.py
@@ -5,3 +5,21 @@ sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 from view.view import  ApplicationView
 from model.API_connection import ZenApi
+
+class AppPresenter:
+    def __init__(self):
+        self.view = ApplicationView() # the main view
+        self.api = ZenApi() # api connection and data processing
+        self.input = "" # Inputs from user
+        self.current_id = -1 # random initialization
+        self.currrent_page = 999  # random initialization
+
+    def run(self): # driver function to run the application
+        self.diplayMainMenu()
+    
+    def getInput(self): # Get user Input 
+        self.input = input()
+    
+    def displayMainMenu(self): # Main menu for user to interact with the application
+        
+ 
